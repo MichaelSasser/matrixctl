ade340844e9840a52a0971815a41eac6d40da4f7
Michael Sasser <Michael@MichaelSasser.org>
2020-04-14 21:26:16 +0200

    Fixed version test

--- tests/test_matrixctl.py
+++ tests/test_matrixctl.py
@@ -1,7 +1,22 @@
 from __future__ import annotations
 
-from app import __version__
+import re
+from matrixctl import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    """Tests, if the version matches SemVer. The regular expression is form:
+    https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    """
+    # Setup
+    desired = re.compile(
+        r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+    )
+
+    # Exercise
+    actual = __version__
+
+    # Verify
+    assert desired.fullmatch(actual)
+
+    # Cleanup - None
