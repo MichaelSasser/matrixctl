cadf5848005ce8a3a94b318fe8724e4d8102a45c
Michael Sasser <Michael@MichaelSasser.org>
2021-02-23 19:57:04 +0100

    typo

--- tests/test_matrixctl.py
+++ tests/test_matrixctl.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+# matrixctl
+# Copyright (c) 2021  Michael Sasser <Michael@MichaelSasser.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import re
@@ -8,7 +24,7 @@ from matrixctl import __version__
 def test_version() -> None:
     """Test, if the version matches SemVer.
 
-    The regular expression is form:
+    The regular expression is from:
     https://semver.org/#is-there-a-suggested-regular-expression-regex-to-
             check-a-semver-string
     """
