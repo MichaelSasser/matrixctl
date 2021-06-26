9a0f7806b25e8daa627b0225ccdb29e132f8d0b7
Michael Sasser <Michael@MichaelSasser.org>
2021-06-26 11:43:36 +0200

    Test RTD: typo: sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ exclude_patterns: List[str] = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme: str = "sphinx-rtd-theme"
+html_theme: str = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
