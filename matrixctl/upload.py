d9f443bb9da04d87628d085b444d0f2a88265b21
Michael Sasser <Michael@MichaelSasser.org>
2021-06-26 17:16:44 +0200

    Rename module typing to types

--- matrixctl/upload.py
+++ matrixctl/upload.py
@@ -31,7 +31,7 @@ from .errors import InternalResponseError
 from .handlers.api import RequestBuilder
 from .handlers.api import request
 from .handlers.toml import TOML
-from .typing import JsonDict
+from .types import JsonDict
 
 
 __author__: str = "Michael Sasser"
