dcef876fb042a66fbd241d6ac8568daf8f7a9415
Michael Sasser <Michael@MichaelSasser.org>
2021-08-01 00:22:52 +0200

    Drop support for Python 3.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,6 @@ import sys
 
 from datetime import date
 from pathlib import Path
-from typing import List
 
 from single_source import get_version
 
@@ -58,7 +57,7 @@ release: str = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions: List[str] = [
+extensions: list[str] = [
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx_autodoc_typehints",
@@ -73,7 +72,7 @@ extensions: List[str] = [
     "sphinx.ext.inheritance_diagram",
 ]
 
-suppress_warnings: List[str] = ["autosectionlabel.*"]
+suppress_warnings: list[str] = ["autosectionlabel.*"]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
@@ -110,12 +109,12 @@ autosummary_generate: bool = True
 autosummary_imported_members: bool = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path: List[str] = ["_templates"]
+templates_path: list[str] = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: List[str] = []
+exclude_patterns: list[str] = []
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -127,4 +126,4 @@ html_theme: str = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path: List[str] = ["_static"]
+html_static_path: list[str] = ["_static"]
